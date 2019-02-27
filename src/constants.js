de9fdd0fa67af51ecc7867b8acc930c56731ba6c
Nithin <nithin.linkin@gmail.com>
2019-02-27 15:59:18 +0530

    [config] Make the key name flavors for uniformity

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ const CONSTANTS = {
     DEFAULT_FLAVOR_THEME: "",
     EXPRESSION_TRAVERSE_LIMIT: 20,
     CONFIG_NAME: "flavorsConfig",
-    CONFIG_FLAVOR_MAP_KEY: "flavorMap",
+    CONFIG_FLAVOR_MAP_KEY: "flavorsMap",
 }
 
 export default CONSTANTS
