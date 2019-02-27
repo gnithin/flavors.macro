6590b1c45380ddc70d42a4493379158c4d421178
Nithin <nithin.linkin@gmail.com>
2019-02-27 13:19:58 +0530

    [example] Remove the stock create-react-app and add use-ful examples

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hello from './hello.js'
+
+// import Hello from './hello.default'
+// import flavors from 'flavors.macro'
+// flavors();
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        console.log("HEL - ", Hello)
+        return (
+            <div className="App">
+                <Hello />
+            </div>
+        );
+    }
 }
 
 export default App;
