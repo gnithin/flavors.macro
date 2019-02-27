ea2967f296b2a131b5ef038685fdd9423405c67b
Nithin <nithin.linkin@gmail.com>
2019-02-27 14:46:53 +0530

    [config] Remove the unnecessary constants value

--- src/constants.js
+++ src/constants.js
@@ -5,14 +5,6 @@ const CONSTANTS = {
     DEFAULT_FLAVOR_THEME: "green",
     EXPRESSION_TRAVERSE_LIMIT: 20,
     CONFIG_NAME: "flavorsConfig",
-
-    // TODO: this needs to be removed
-    DEFAULT_CONFIG_VAL: {
-        "flavorsMap": {
-            "default": "green",
-            "layout": "red",
-        }
-    }
 }
 
 export default CONSTANTS
