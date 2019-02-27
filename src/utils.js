958530fa6b47d92c6cb9f88f133f8122972673d0
Nithin <nithin.linkin@gmail.com>
2019-02-27 15:34:44 +0530

    [config] Add escape-regex utility method

--- src/utils.js
+++ src/utils.js
@@ -10,4 +10,18 @@ export default class Utils {
     static isEmptyObj(obj) {
         return this.isNull(obj) || Object.entries(obj).length === 0
     }
+
+    /**
+     * Escape regex related symbols from string.
+     * NOTE: Picked up from here - https://stackoverflow.com/a/6969486/1518924
+     * Can't believe JS does not have an in-built method for this. Scratch that, I can really believe that :p
+     * @param {string}
+     * @returns {string}
+     */
+    static escapeRegExp(str) {
+        if (typeof str !== "string") {
+            return str
+        }
+        return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+    }
 }
